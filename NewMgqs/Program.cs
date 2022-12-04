ae2f586f56ec7884d340de5629dc5fd57dfecb50
The Geek Techie <94162832+Thegeektechie@users.noreply.github.com>
2022-12-04 10:27:53 +0100

    Update Program.cs

--- NewMgqs/Program.cs
+++ NewMgqs/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+internal class Program
+{
+            private static string getNewA(string part1, string part2)
+            {
+                int length = part2.Length();
+                StringBuilder builder = new StringBuilder(length);
+                for (var i = 0; i < length; i++)
+                {
+                 
+                }
+                return builder.ToString();
+            }
+            public static void main(String[] args)
+            {
+                var input = "i am a boy now in the university";
+                var middle = input.Length() / 2;
+                var part1 = input.Substring(0, middle);
+                var part2 = input.Substring(middle);
+                var newA = getNewA(part1, part2);
+                Console.WriteLine(newA);
+            }
+        }
+   
