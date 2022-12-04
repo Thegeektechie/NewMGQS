238735b3583958b8bc1e31c72dd3fa16b9cf395b
The Geek Techie <94162832+Thegeektechie@users.noreply.github.com>
2022-12-04 10:24:54 +0100

    Update JS-solution.js

--- NewMgqs/JavaScript Solution/JS-solution.js
+++ NewMgqs/JavaScript Solution/JS-solution.js
@@ -5,5 +5,15 @@ function solve(str){
     var pivot = Math.floor(str.length / 2);
     var str1 = str.substring(0, pivot).split("");
     var str2 = str.substring(pivot).split("");
-        
+
+    for (var i = 0; i < str1.length; i++){
+        if (i % 2 !==0) {
+            temp = str1[i]
+            str1[i] = str2[i - 1];
+            str2[i -1] - temp;
+        }
     }
+        return str2.join("") + str1.join("");
+    }
+
+    console.log(solve("AfricanJournal"));
